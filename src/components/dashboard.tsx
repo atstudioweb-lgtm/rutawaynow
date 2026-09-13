4c846f0ef7685d4e180f74eba82d3c97ee5069b6
atstudioweb <atstudioweb@gmail.com>
2026-09-12 21:02:47 -0300

    fix: add useRef import for map scroll

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Icon } from "@/components/icons";
 import { ChecklistModal } from "@/components/checklist-modal";
 import { LanguageSwitcher } from "@/components/language-switcher";
